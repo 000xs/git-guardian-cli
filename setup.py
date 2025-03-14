3b7bcd7e2e6deff39b4be30086b7adf7b5205df3
Sithum Sathsara <89584440+SithumSathsaras@users.noreply.github.com>
2025-03-14 19:40:10 -0400

    Prepare for release v1.0.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="git-guardian-cli",
-    version="0.0.1",
+    version="1.0.1",
     description="A CLI tool to scan Git repositories for sensitive information",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
